d1980307c701de629107e91fe98ab20ae88e7f8e
Christian_git_hub <christianipc.1987@gmail.com>
2021-05-14 01:07:38 -0300

    Docente-Comunicado, inicio con la estructura de la pagina,titulos, textos y botones. todo responsive

---
+++ src/components/Docente/DocenteComunicado.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import NavDocente from '../NavDocente'
+import '../../assets/comunicado.css'
+
+const DocenteComunicado = () => {
+    return(
+        <React.Fragment>
+            <NavDocente></NavDocente>  
+            <div className="container-fuid comunicado">
+                <div className="d-sm-flex h-100">
+                    <div className="menu-lateral col-12 col-md-2 pt-3 border-right d-none d-md-block" > 
+                        <ul className="nav flex-column justify-content-start ">
+                            <li className="nav-item">
+                                <a className="nav-link text-dark" href="/#">Actividad Reciente</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-dark font-weight-bold" href="/#">Cursos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-dark" href="/#">Calificaciones</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="docente-comunicado-cuerpo col-12 col-md-9 pt-3">
+                        <h2 className="docente__titulo pb-2 font-weight-bold">Escuela Primaria N° 12 - Primero C</h2>
+                        <div className="comunicado__nav mb-3">
+                            <ul className="nav flex-row justify-content-start">
+                                <li className="nav-item">
+                                    <a className="nav-link text-dark" href="/#">Estudiantes</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link text-dark" href="/#">Materia</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="/#">Comunicados</a>
+                                </li>
+                            </ul> 
+                        </div>
+                         
+                        <div className="comunicado__contenido d-sm-flex border">
+                            <div className="comunicado__contenido--navbar border-right border-bottom pt-3 pl-0 pr-0 col-12 col-sm-3 col-lg-2">
+                                <ul className="nav flex-column justify-content-start p-0 text-center">
+                                    <li className="nav-item pl-2 pr-2 pl-lg-3 pr-lg-3 pt-1 pb-1">
+                                        <button type="button" class="btn btn-outline-primary btn-lg pl-3 pr-3 pt-1 pb-1">Nuevo +</button>
+                                    </li>
+                                    <li className="nav-item mt-4"  Style="background-color:#eee">
+                                        <a className="nav-link pl-0" href="/#">Grupales</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link pl-0" href="/#">Particulares</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="comunicado_contenido--info w-md-75 p-4 p-sm-5">
+                                <article className="pb-3">
+                                    <h3 className="font-weight-bold">Acto día de la familia</h3>
+                                    <p>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere assumenda minima sed ducimus, 
+                                        consequatur ab repudiandae libero ullam eos asperiores debitis, consectetur nesciunt labore autem 
+                                        ut explicabo ratione. Illo, est.
+                                    </p>
+                                </article>
+                                <article>
+                                    <h3 className="font-weight-bold">Actividades de invierno</h3>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae nemo quia perspiciatis fuga 
+                                        error cum iusto numquam, possimus tempore velit et, sint temporibus aut ex impedit iure maxime 
+                                        veritatis libero.
+                                    </p>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+
+export default DocenteComunicado;
