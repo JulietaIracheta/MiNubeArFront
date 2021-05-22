f00182547a540183792a1c0d8066c2d4c84ba8cb
Christian_git_hub <christianipc.1987@gmail.com>
2021-05-22 13:15:18 -0300

    (#3)(#2) agrego los cambios del Router en el App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import Docente from './components/Docente/Page/index.js';
 import EstudiantesAsignados from './components/Estudiante/EstudiantesAsignados';
 import PerfilEstudiante from './components/Estudiante/PerfilEstudiante';
 import CursosAsignadosDocente from './components/Docente/CursosAsignadosDocente'
+import DocenteComunicado from './components/Docente/DocenteComunicado';
+import Instituciones from './components/Instituciones/Instituciones';
+import InstitucionesEditar from './components/Instituciones/InstitucionesEditar';
+import Cursos from './components/Admin/Cursos';
+import CursosEditar from './components/Admin/CursosEditar'
+
 function App() {
   return (
     <Router>
@@ -17,6 +23,11 @@ function App() {
         <Route path='/docente/estudiantesAsignados' exact component={EstudiantesAsignados} />        
         <Route path='/estudiante/:id' exact component={PerfilEstudiante}/>
         <Route path='/curso/:id' exact component={CursosAsignadosDocente}/>
+        <Route path='/docente/comunicados' exact component={DocenteComunicado}/>
+        <Route path='/instituciones/admin' exact component={Instituciones}/>
+        <Route path='/instituciones/admin_editar' exact component={InstitucionesEditar}/>
+        <Route path='/admin/cursos' exact component={Cursos}/>
+        <Route path='/admin/cursos_editar' exact component={CursosEditar}/>
       </Switch>
     </Router>
 
