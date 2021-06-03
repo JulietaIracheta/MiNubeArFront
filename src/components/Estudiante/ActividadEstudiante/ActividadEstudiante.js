6aaea7a8d8bed6dfd7059621686409d9a8f2a8ff
marcoscabralg <mcabral@globons.com>
2021-06-02 23:30:20 -0300

    estilos a videoConference, y reproductor de video

--- src/components/Estudiante/ActividadEstudiante/ActividadEstudiante.js
+++ src/components/Estudiante/ActividadEstudiante/ActividadEstudiante.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactApexChart from 'react-apexcharts'
 import Chart from '../../Chart/chart';
 import NavTutor from '../../Tutor/NavTutor';
 import '../../../assets/css/css-actividad-estudiante.css'
 import BotonVolver from "../../BotonVolver/botonVolver";
 import Sidebar from "../../Sidebar";
-
+import ReactPlayer from 'react-player'
+import Video from '../../Contenido/Videos/video.mp4'
 export default function ApexChart() {
     const chart = Chart({ series: 56 });
-    console.log(chart)
+    const [video,setVideo]=useState([]);
+    const urlBase = 'http://localhost:60671/api/contenido/?id=2'
+    const [videoFilePath, setVideoFilePath] = useState(null);
+
+    var urlVideo='/videos/';
+    useEffect(function(){
+        fetch(urlBase,{
+            method: 'GET'
+        }).then(res => {
+            if (!res.ok) alert('video no encontrado')
+            return res.json()
+        }).then(res => {
+            setVideoFilePath(urlVideo+video.video);
+            setVideo(res);
+        });
+    },[]);
+    urlVideo=urlVideo+video.video;
     return (
         <div>
             <NavTutor></NavTutor>
@@ -24,7 +41,10 @@ export default function ApexChart() {
                         >Matemáticas
                         </h3>
                     </div>
+                    <ReactPlayer url={urlVideo} controls width="500px" height="300px" />
+
                     <div className="row mt-1">
+
                         <div className="col-md-6 d-flex align-items-center flex-column">
                             <p className="font-weight-bold">Clases vistas</p>
                             <div id="chart">
